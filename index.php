30a2587778495971a3bda74d0232b8115f3c69b0
Jared Eddy <jaredgeddy@gmail.com>
2018-02-06 08:55:36 -0800

    Completed Episode 3 - Variables in PHP.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
-  echo '"You don\'t have to be great at something to get started but you do have to get started in order to be great at something." - Zig Ziglar';
+  $inspirationalQuote = '"You don\'t have to be great at something to get started but you do have to get started in order to be great at something."';
+  $quoteAuthor = 'Zig Ziglar';
+
+  echo $inspirationalQuote . ' - ' . $quoteAuthor;
 
 ?>
