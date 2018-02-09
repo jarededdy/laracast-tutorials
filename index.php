e5a04ae51b0edec8b45a5f2136233a0f9b13df42
Jared Eddy <jaredgeddy@gmail.com>
2018-02-09 13:28:23 -0800

    completed lesson 5 php and html

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <style>
 
-  $inspirationalQuote = '"You don\'t have to be great at something to get started but you do have to get started in order to be great at something."';
-  $quoteAuthor = 'Zig Ziglar';
+      header {
+        background: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+      }
 
-  echo $inspirationalQuote . ' - ' . $quoteAuthor;
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1>
+        <?php
 
-?>
+          //idea - create a madlibs using user input and variables. 
+
+          $inspirationalQuote = '"You don\'t have to be great at something to get started but you do have to get started in order to be great at something."';
+          $quoteAuthor = 'Zig Ziglar';
+
+          $name = htmlspecialchars($_GET['name']);
+
+          echo 'Hello, ' . $name . '. Did you know that ' . $quoteAuthor . ' once said, ' . $inspirationalQuote;
+
+        ?>
+      </h1>
+
+      <!-- <h2>
+        <?= "Hello, " . htmlspecialchars($_GET['name']) . "!"; ?> //sanatizing our input using htmlspecialchars
+      </h2> -->
+    </header>
+  </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,8 @@
+<?php
+
+  $inspirationalQuote = '"You don\'t have to be great at something to get started but you do have to get started in order to be great at something."';
+  $quoteAuthor = 'Zig Ziglar';
+
+  echo $inspirationalQuote . ' - ' . $quoteAuthor;
+
+?>
